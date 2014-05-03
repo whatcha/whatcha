771ddd354b24fec0b71190ee01cfddf5f406bc9b
Mark Woodall <mawoodall@gmail.com>
2014-05-02 23:15:59 -0500

    working memory blog

--- web.js
+++ web.js
@@ -1,28 +1,71 @@
+
+/**
+ * Module dependencies.
+ */
+
+var render = require('./lib/render');
+var logger = require('koa-logger');
+var route = require('koa-route');
+var parse = require('co-body');
 var koa = require('koa');
 var app = koa();
 
-// x-response-time
+// "database"
 
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  this.set('X-Response-Time', ms + 'ms');
-});
+var posts = [];
 
-// logger
+// middleware
 
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
-});
+app.use(logger());
 
-// response
+// route middleware
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
+app.use(route.get('/', list));
+app.use(route.get('/post/new', add));
+app.use(route.get('/post/:id', show));
+app.use(route.post('/post', create));
+
+// route definitions
+
+/**
+ * Post listing.
+ */
+
+function *list() {
+  this.body = yield render('list', { posts: posts });
+}
+
+/**
+ * Show creation form.
+ */
+
+function *add() {
+  this.body = yield render('new');
+}
+
+/**
+ * Show post :id.
+ */
+
+function *show(id) {
+  var post = posts[id];
+  if (!post) this.throw(404, 'invalid post id');
+  this.body = yield render('show', { post: post });
+}
+
+/**
+ * Create a post.
+ */
+
+function *create() {
+  var post = yield parse(this);
+  var id = posts.push(post) - 1;
+  post.created_at = new Date;
+  post.id = id;
+  this.redirect('/');
+}
+
+// listen
 
 app.listen(3000);
+console.log('listening on port 3000');
